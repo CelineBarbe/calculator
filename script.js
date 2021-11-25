7da776650db0ef679724f351d337653c119af59f
CelineBarbe <laloutrecosmique@hotmail.fr>
2021-11-25 15:34:38 +0100

    step 4 and 5

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
 function button1Clicked() {
     let screen = document.getElementById('screen');
-    screen.textContent += 1;
+    screen.textContent += "1";
 };
 function button2Clicked() {
     let screen = document.getElementById('screen');
-    screen.textContent += 2;
+    screen.textContent += "2";
 };
 function button3Clicked() {
     let screen = document.getElementById('screen');
-    screen.textContent += 3;
+    screen.textContent += "3";
 };
 function button4Clicked() {
     let screen = document.getElementById('screen');
-    screen.textContent += 4;
+    screen.textContent += "4";
 };
 function button5Clicked() {
     let screen = document.getElementById('screen');
-    screen.textContent += 5;
+    screen.textContent += "5";
 };
 function button6Clicked() {
     let screen = document.getElementById('screen');
-    screen.textContent += 6;
+    screen.textContent += "6";
 };
 function button7Clicked() {
     let screen = document.getElementById('screen');
-    screen.textContent += 7;
+    screen.textContent += "7";
 };
 function button8Clicked() {
     let screen = document.getElementById('screen');
-    screen.textContent += 8;
+    screen.textContent += "8";
 };
 function button9Clicked() {
     let screen = document.getElementById('screen');
-    screen.textContent += 9
+    screen.textContent += "9";
 };
 function buttonPlusClicked() {
     let screen = document.getElementById('screen');
     screen.textContent += "+";
+};
+function buttonMoinsClicked() {
+    let screen = document.getElementById('screen');
+    screen.textContent += "-";
+}
+
+function solve() {
+    let equation = document.getElementById('screen').textContent;
+    let solved = eval(equation);
+    document.getElementById('screen').innerHTML = solved;
+};
+function clearScreen() { 
+    let screen = document.getElementById('screen');
+    screen.textContent = "";
 }
