87b98c63dde2793c8c49237853ec2a7ba9d7bba6
CelineBarbe <laloutrecosmique@hotmail.fr>
2021-11-25 12:01:23 +0100

    step 2 and 3

---
+++ script.js
@@ -0,0 +1,40 @@
+function button1Clicked() {
+    let screen = document.getElementById('screen');
+    screen.textContent += 1;
+};
+function button2Clicked() {
+    let screen = document.getElementById('screen');
+    screen.textContent += 2;
+};
+function button3Clicked() {
+    let screen = document.getElementById('screen');
+    screen.textContent += 3;
+};
+function button4Clicked() {
+    let screen = document.getElementById('screen');
+    screen.textContent += 4;
+};
+function button5Clicked() {
+    let screen = document.getElementById('screen');
+    screen.textContent += 5;
+};
+function button6Clicked() {
+    let screen = document.getElementById('screen');
+    screen.textContent += 6;
+};
+function button7Clicked() {
+    let screen = document.getElementById('screen');
+    screen.textContent += 7;
+};
+function button8Clicked() {
+    let screen = document.getElementById('screen');
+    screen.textContent += 8;
+};
+function button9Clicked() {
+    let screen = document.getElementById('screen');
+    screen.textContent += 9
+};
+function buttonPlusClicked() {
+    let screen = document.getElementById('screen');
+    screen.textContent += "+";
+}
